5258f4481d78da187e80d0f8a6b9ffc76f61e341
JoshuaDalida <141461365+JoshuaDalida@users.noreply.github.com>
2024-09-12 18:23:08 -0600

    made the nav bar

---
+++ cine/components/HomeWidget.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeWidget = () => {
+    return (
+        <View>
+            <Text>HomeWidget</Text>
+        </View>
+    )
+}
+
+export default HomeWidget
