08371566f1bfe00718fedfa78dd424885dd2d00f
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-01 17:20:41 -0600

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-"use client"
+"use client";
+
 import { UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import Page from "./pages/page";
-
 
 export default function Home() {
   const { isSignedIn } = useUser();
 
   return (
     <div>
-    {isSignedIn && <Page/>}
-    <div>
-      {!isSignedIn ? (
-        <div>
-          <Link href="/sign-up">Signup</Link>
-          <Link href="/sign-in">Login</Link>
-        </div>
-      ) : (
-        <UserButton/>
-      )}
+      {isSignedIn && <Page />}
+      <div>
+        {!isSignedIn ? (
+          <div>
+            <Link href="/sign-up">Signup</Link>
+            <Link href="/sign-in">Login</Link>
+          </div>
+        ) : (
+          <UserButton />
+        )}
+      </div>
     </div>
-  </div>
   );
 }
