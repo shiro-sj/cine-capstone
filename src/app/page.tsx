077597468a0bb6dd715e8c8e5f07045d3ecaf27b
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-01 17:16:57 -0600

    clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client"
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import Page from "./pages/page";
+
+
 export default function Home() {
+  const { isSignedIn } = useUser();
+
   return (
     <div>
-      Home page
+    {isSignedIn && <Page/>}
+    <div>
+      {!isSignedIn ? (
+        <div>
+          <Link href="/sign-up">Signup</Link>
+          <Link href="/sign-in">Login</Link>
+        </div>
+      ) : (
+        <UserButton/>
+      )}
     </div>
+  </div>
   );
 }
