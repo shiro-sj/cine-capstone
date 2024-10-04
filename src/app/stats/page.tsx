a525ccebd46850dfa6b4612160a85fb1a2f1931c
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-04 16:12:32 -0600

    fix

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -11,69 +11,10 @@ function page() {
         <h1>Stats</h1>
       </div>
 
-      {/* Watch History */}
-      <div className="flex flex-col items-center justify-center">
-        <WatchHistory />
-      </div>
-
-      {/* Year 2024 Section */}
-      <div className="flex flex-col items-center justify-center gap-10">
-        <h2 className="text-xl text-white">2024 Stats</h2>
-
-        {/* Day Graph for 2024 */}
-        <div className="flex flex-col items-center justify-center">
-          <DayGraph year={2024} />
-        </div>
-
-        {/* Weekday Graph for 2024 */}
-        <div className="flex flex-col items-center justify-center">
-          <WeekDayGraph year={2024} />
-        </div>
-      </div>
-
-      {/* Year 2023 Section */}
-      <div className="flex flex-col items-center justify-center gap-10">
-        <h2 className="text-xl text-white">2023 Stats</h2>
-
-        {/* Day Graph for 2023 */}
-        <div className="flex flex-col items-center justify-center">
-          <DayGraph year={2023} />
-        </div>
-
+      <div>
         {/* Weekday Graph for 2023 */}
         <div className="flex flex-col items-center justify-center">
-          <WeekDayGraph year={2023} />
-        </div>
-      </div>
-
-      {/* Year 2023 Section */}
-      <div className="flex flex-col items-center justify-center gap-10">
-        <h2 className="text-xl text-white">2022 Stats</h2>
-
-        {/* Day Graph for 2023 */}
-        <div className="flex flex-col items-center justify-center">
-          <DayGraph year={2022} />
-        </div>
-
-        {/* Weekday Graph for 2023 */}
-        <div className="flex flex-col items-center justify-center">
-          <WeekDayGraph year={2022} />
-        </div>
-      </div>
-
-
-      {/* Year 2023 Section */}
-      <div className="flex flex-col items-center justify-center gap-10">
-        <h2 className="text-xl text-white">2020 Stats</h2>
-
-        {/* Day Graph for 2023 */}
-        <div className="flex flex-col items-center justify-center">
-          <DayGraph year={2020} />
-        </div>
-
-        {/* Weekday Graph for 2023 */}
-        <div className="flex flex-col items-center justify-center">
-          <WeekDayGraph year={2020} />
+          <WeekDayGraph/>
         </div>
       </div>
       
