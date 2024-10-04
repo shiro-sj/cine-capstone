484b3dcbfb45313e5a3357ff2304b5f52c7071e5
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-04 02:11:33 -0600

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
