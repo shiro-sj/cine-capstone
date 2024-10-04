b1ca1dddeba6d04230aa37b095d223e3d7dcd907
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-03 20:53:10 -0600

    still fixing

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import { currentUser } from '@clerk/nextjs/server';
-import prisma from '../../../lib/prisma';
+
 
 export default async function Home() {
-  const user = await currentUser();
-    if(!user){
-        return null;
-    }
-
-    const loggedInUser = await prisma.user.findUnique({
-        where:{ clerkId: user.id, email: user.emailAddresses[0].emailAddress}
-    });
-    if(!loggedInUser){
-      await prisma.user.create({
-        data:{
-          clerkId: user.id,
-          username: user.username || 'No username available.',
-          email: user.emailAddresses[0].emailAddress
-        }
-      })
-    }
-
   return (
     <div className="bg-gradient-to-br from-dark-purple to-slate-800 min-h-screen flex flex-col items-center justify-center gap-40">
       
