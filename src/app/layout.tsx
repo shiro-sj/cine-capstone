c8efad291cc9b97f644456620aef8631da772553
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-15 13:05:39 -0600

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import NavBar from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: "cine",
