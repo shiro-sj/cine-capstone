45af51bf8f616e2d8d6eb2792bc2917abafd8cf0
cam_sanjuan <shiro.sanjuanm@gmail.com>
2024-10-22 18:10:30 -0600

    case 3 logic

--- src/app/protected/upload/csvUploader.tsx
+++ src/app/protected/upload/csvUploader.tsx
@@ -16,6 +16,18 @@ export default function CSVUploader() {
 
   const [csvData, setCsvData] = useState<{ title: string; watchedAt: string; isTvShow: boolean; id: string; data:{} }[]>([]);
 
+  //function for comparing strings (using jaccard similarity index)
+  function jaccardCompare(str1: string, str2: string){
+    let string1 = str1.toLowerCase().split(/\W+/)
+    let string2 = str2.toLowerCase().split(/\W+/)
+    let intersection = new Set([...string1].filter(x=>string2.includes(x)));
+    let union = new Set([...string1, ...string2]);
+
+    let similarity = intersection.size / union.size;
+
+    return similarity;
+}
+
   async function searchMovieID(searchUrl: string, title:string){
     try {
       const response = await axios.get(`${searchUrl}/movie`,{
@@ -38,7 +50,12 @@ export default function CSVUploader() {
           query:title
         }
       })
-      return response.data.results[0].id;
+
+      for (let result in response.data.results){
+        if (title === response.data.results[result].name || title === response.data.results[result].original_name){
+          return response.data.results[result].id
+        }
+      }
     } catch (e) {
       console.error('Error fetching series ID: ',e)
     }
@@ -48,15 +65,16 @@ export default function CSVUploader() {
   async function findSeasons(findUrl:String, id:string|null){
     try{
      const response = await findSeriesByID(findUrl, id)
+     const seasonNumbers = response.seasons.map((x: { season_number: any; }) => x.season_number);
 
-     return response.seasons;
+     return seasonNumbers;
 
     }catch(e){
       console.log('Error fetching series data: ', e)
     }
   };
 
-  async function findSeasonName(findUrl:String, id: string|null, season: number, seasonName: string){
+  async function findSeasonData(findUrl:String, id: string|null, season: string){
     try {
       const response = await axios.get(`${findUrl}/tv/${id}/season/${season}`, {params:{
         api_key:TMDB_API_KEY
@@ -101,7 +119,7 @@ export default function CSVUploader() {
 
   };
 
-  async function findEpisode(findUrl:String, id:string | null, episodeName:String, season: String){
+  async function findEpisode(findUrl:String, id:string | null, episodeName:string, season: string|null){
     try {
       const response = await axios.get(`${findUrl}/tv/${id}/season/${season}`,{
         params:{
@@ -111,7 +129,8 @@ export default function CSVUploader() {
 
       for (let episodeNumber in response.data.episodes){
         let name = response.data.episodes[episodeNumber].name
-        if (name.includes(episodeName)){
+        const index = jaccardCompare(name, episodeName);
+        if (index>=0.5){
           return response.data.episodes[episodeNumber];
         }
       }
@@ -171,54 +190,86 @@ const parseCSV = (content: string) => {
                   break;
                 case 3:
                   try {
-                  title = parts[0].trim();
-                  episodeName = parts[2].trim();
-                  id = await searchSeriesID(searchUrl, title)
+                    title = parts[0].trim();
+                    episodeName = parts[2].trim();
+                    id = await searchSeriesID(searchUrl, title)
+            
+                    if (id){
+                      const seasonNumber= parts[1].trim().match(/\d+/);
+                      if (seasonNumber){
+                        season = seasonNumber[0]
+                        console.log(await findEpisode(findUrl, id , episodeName, season)) 
+                      }else{
+                        title = parts[0].trim();
+                        episodeName = parts[2].trim()
+                        const seasonName = parts[1].trim();                    
+                        id = await searchSeriesID(searchUrl, title);
 
-                  if (id){
-                    const seasonNumber= parts[1].trim().match(/\d+/);
-                    if (seasonNumber){
-                      season = seasonNumber[0]
-                      console.log(await findEpisode(findUrl, id , episodeName, season)) 
-                    }else{
-                      title = parts[0].trim();
-                      episodeName = parts[2].trim()
-                      const seasonName = parts[1].trim();                    
-                      id = await searchSeriesID(searchUrl, title);
-                      // console.log(await findSeriesByID(findUrl, id))
-                      // console.log(await findSeasons(findUrl, id));
-                      const seasons = await findSeasons(findUrl, id)
+                        const seasons = await findSeasons(findUrl, id)                 
 
-                      for (let i = 0; i< seasons.length ; i++){
-                        const data = await findSeasonName(findUrl, id, i, seasonName)
-                        console.log(data);
-                        
-                        
+                        for (let i of seasons){
+                          try {
+                            const data = await findSeasonData(findUrl, id, i);
 
-                        // if (await findEpisode(findUrl, id, episodeName, seasonNumber)){
-                        //   const episodeData = findEpisode(findUrl, id, episodeName, seasonNumber)
-                        //   console.log(episodeData);
-                        // } else {
-                        //   continue
-                        // }
-                      } 
-                      
-
-                      // console.log(findEpisode(findUrl, id, episodeName, season))
+                            if (seasons.length < 2){
+                              season = data.season_number;
+                              const episodeData = await findEpisode(findUrl, id, episodeName, season);
+                              console.log(episodeData);
+                            } else{
+                              if (!data) {
+                                continue;
+                              } else {
+                                let str1 = data.name;
+                                let str2 = seasonName;
+                                const index = jaccardCompare(str1, str2);
+                            
+                                if (index >= 0.8) {
+                                  season = data.season_number;
+                                  const episodeData = await findEpisode(findUrl, id, episodeName, season);
+                                  console.log(episodeData);
+                                }
+                              }
+                            }
+                          } catch (error) {
+                            console.error(`Error processing season at index ${i}:`, error);
+                            continue; 
+                          }
+                        } 
+                      }
+                    } else{
+                      title = row.Title.trim();
+                      id = await searchMovieID(searchUrl, title)
+                      console.log(await findMovieByID(findUrl,id))
                     }
-                  } 
                   } catch (error) {
                     console.log(error)
-                    
+                  }
+                  break;
+                case 4:
+                  title = parts[0].trim();
+                  episodeName = parts[3].trim();
+                  id = await searchSeriesID(searchUrl, title)
+          
+                  if (id){
+                    const seasonNumber= parts[1].trim().match(/\d+/)?.toString()
+                    const seasons = await findSeasons(findUrl, id)
+                    if (seasonNumber){
+                      const seasonData = await findSeasonData(findUrl, id, '4')
+                      // console.log(seasonData)
+                      // console.log(seasons)
+
+                      const episodeData = await findEpisode(findUrl, id, episodeName, seasonNumber)
+                      console.log(episodeData)
+                    } else{
+                      // if (seasons > 1){
+                        
+
+                      // }
+                    }
                   }
                   
                   break;
-                case 4:
-                  break;
-              
                }
-
-              
 
             } else {
               title = row.Title.trim();
