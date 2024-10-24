74fcf74ac64cc6958798662e218135474d0cec90
JoshuaDalida <joshuamdalida@gmail.com>
2024-10-24 16:42:53 -0600

    search styling

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,19 +1,23 @@
-import { UserButton } from '@clerk/nextjs'
+import { UserButton,useUser } from '@clerk/nextjs'
 import React from 'react'
 import CSVUploader from './upload/csvUploader'
 import ChatBot from '../../components/chatBot'
 import SearchBar from '../../components/searchBar'
-import { useAuth } from '@clerk/nextjs'
+
 
 export default function Home(){
+
+    const {user} = useUser();
 
     return(
         <div className='container mx-auto'>
 
             <UserButton/>
+            <a href= {`/profile/${user?.username}`}>profile</a>
             <SearchBar/>
             <CSVUploader/>
             <ChatBot/>
+
         </div>
     )
 };
